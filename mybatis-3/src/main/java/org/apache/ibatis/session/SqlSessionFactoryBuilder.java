8eb38b72b1ed56705ef47c0554655db16befef37
330059579 <wyunlong@yeah.net>
2020-05-07 17:33:40 +0800

    commit

---
+++ mybatis-3/src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
@@ -0,0 +1,104 @@
+/**
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.session;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Properties;
+
+import org.apache.ibatis.builder.xml.XMLConfigBuilder;
+import org.apache.ibatis.exceptions.ExceptionFactory;
+import org.apache.ibatis.executor.ErrorContext;
+import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
+
+/**
+ * Builds {@link SqlSession} instances.
+ *
+ * @author Clinton Begin
+ */
+public class SqlSessionFactoryBuilder {
+
+  public SqlSessionFactory build(Reader reader) {
+    return build(reader, null, null);
+  }
+
+  public SqlSessionFactory build(Reader reader, String environment) {
+    return build(reader, environment, null);
+  }
+
+  public SqlSessionFactory build(Reader reader, Properties properties) {
+    return build(reader, null, properties);
+  }
+
+
+  //所有的build()方法最终调用的就是此方法
+  public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
+    try {
+      //1.根据所传参数，获得对应的配置XMLConfigBuilder类， 入参是Config.xml的配置文件
+      XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
+      /*
+      2.parser.parse() 返回的是配置信息，调用build(Configuration config) 直接将Configuration设置到DefaultSqlSessionFactory类中
+      此时parser中已经有了被解析的配置文件数据，和一个new 出的Configuration类，真正Configuration构建逻辑就在XMLConfigBuilder.parse()中
+      ，parse()进行了一套风骚操作以后，使我们到一个又有的Configuration类，set到DefaultSqlSessionFactory中返回
+      */
+      return build(parser.parse());
+    } catch (Exception e) {
+      throw ExceptionFactory.wrapException("Error building SqlSession.", e);
+    } finally {
+      //TODO 清空当前线程中数据
+      ErrorContext.instance().reset();
+      try {
+        reader.close();
+      } catch (IOException e) {
+        // Intentionally ignore. Prefer previous error.
+      }
+    }
+  }
+
+  public SqlSessionFactory build(InputStream inputStream) {
+    return build(inputStream, null, null);
+  }
+
+  public SqlSessionFactory build(InputStream inputStream, String environment) {
+    return build(inputStream, environment, null);
+  }
+
+  public SqlSessionFactory build(InputStream inputStream, Properties properties) {
+    return build(inputStream, null, properties);
+  }
+
+  public SqlSessionFactory build(InputStream inputStream, String environment, Properties properties) {
+    try {
+      XMLConfigBuilder parser = new XMLConfigBuilder(inputStream, environment, properties);
+      return build(parser.parse());
+    } catch (Exception e) {
+      throw ExceptionFactory.wrapException("Error building SqlSession.", e);
+    } finally {
+      ErrorContext.instance().reset();
+      try {
+        inputStream.close();
+      } catch (IOException e) {
+        // Intentionally ignore. Prefer previous error.
+      }
+    }
+  }
+
+  public SqlSessionFactory build(Configuration config) {
+    return new DefaultSqlSessionFactory(config);
+  }
+
+}
